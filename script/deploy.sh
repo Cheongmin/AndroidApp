f1ae8569d7135285ded51c63c0407122b6206e32
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2018-08-31 15:57:29 +0900

    Enable skip_cleanup option

--- script/deploy.sh
+++ script/deploy.sh
@@ -11,5 +11,5 @@ curl \
   -F "token=$SLACK_KEY" \
   -F "channels=$CHANNEL_NAME" \
   -F "initial_comment=$SLACK_TEXT" \
-  -F file=@"app/build/outputs/apk/debug/app-debug.apk" \
+  -F "file=@app/build/outputs/apk/debug/app-debug.apk" \
   https://slack.com/api/files.upload
