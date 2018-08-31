af86d93388ca87af65167c3462195223bfa0cae7
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2018-08-31 14:25:34 +0900

    Add apk write permission

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,9 +5,11 @@ export DEPLOY_COMMIT="$TRAVIS_COMMIT"
 SLACK_TEXT="[ \`$DEPLOY_BRANCH\` | \`$DEPLOY_COMMIT\` ] ${TRAVIS_COMMIT_MESSAGE:-none} "
 CHANNEL_NAME="development"
 
+chmod +x "app/build/outputs/apk/debug/app-debug.apk"
+
 curl \
   -F "token=$SLACK_KEY" \
   -F "channels=$CHANNEL_NAME" \
   -F "initial_comment=$SLACK_TEXT" \
-  -F file="@$(pwd)/app/build/outputs/apk/debug/app-debug.apk" \
+  -F file="@app/build/outputs/apk/debug/app-debug.apk" \
   https://slack.com/api/files.upload
