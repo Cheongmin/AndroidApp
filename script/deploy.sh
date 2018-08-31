30fcd874c0f63a3c466f0cc2c1f15c56196ce309
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2018-08-31 14:14:28 +0900

    Change apk path to absolute

--- script/deploy.sh
+++ script/deploy.sh
@@ -9,5 +9,5 @@ curl \
   -F "token=$SLACK_KEY" \
   -F "channels=$CHANNEL_NAME" \
   -F "initial_comment=$SLACK_TEXT" \
-  -F file=@app/build/outputs/apk/debug/app-debug.apk \
+  -F file="@$(pwd)/app/build/outputs/apk/debug/app-debug.apk" \
   https://slack.com/api/files.upload
