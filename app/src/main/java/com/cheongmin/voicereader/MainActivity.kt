97776050e98a45628cd480ebbcdae9bb83186d8b
Raye <okps123@naver.com>
2018-09-01 19:17:01 +0900

    Implement viewpager with bottom navigation in main activity

--- app/src/main/java/com/cheongmin/voicereader/MainActivity.kt
+++ app/src/main/java/com/cheongmin/voicereader/MainActivity.kt
@@ -2,8 +2,9 @@ package com.cheongmin.voicereader
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupActionBar()
+        setupViewPager()
+        setupBottomNavigationView()
+    }
+
+    private fun setupActionBar() {
         setSupportActionBar(toolbar)
 
         supportActionBar?.run {
@@ -19,36 +26,50 @@ class MainActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
             setHomeAsUpIndicator(R.drawable.ic_menu)
         }
-
-        bottomNavigationView
-                .setOnNavigationItemSelectedListener {
-                    when(it.itemId) {
-                        R.id.navigate_home -> {
-                            replaceFragment(MainHomeFragment.getInstance())
-                            return@setOnNavigationItemSelectedListener true
-                        }
-                        R.id.navigate_log -> {
-                            replaceFragment(MainLogFragment.getInstance())
-                            return@setOnNavigationItemSelectedListener true
-                        }
-                        R.id.navigate_setting -> {
-                            replaceFragment(MainSettingFragment.getInstance())
-                            return@setOnNavigationItemSelectedListener true
-                        }
-                    }
-                    false
-                }
-
-        replaceFragment(MainHomeFragment.getInstance())
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager
-                .beginTransaction()
-                .run {
-                    replace(frameLayout.id, fragment)
-                    addToBackStack(null)
-                    commit()
+    private fun setupViewPager() {
+        viewPager?.adapter = object: FragmentPagerAdapter(supportFragmentManager) {
+            override fun getItem(position: Int): Fragment {
+                when(position) {
+                    0 -> return MainHomeFragment.getInstance()
+                    1 -> return MainLogFragment.getInstance()
+                    2 -> return MainSettingFragment.getInstance()
                 }
+
+                return MainHomeFragment.getInstance()
+            }
+
+            override fun getCount(): Int = 3
+        }
+
+        viewPager?.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                bottomNavigationView.menu.getItem(position).apply {
+                    isChecked = true
+                    supportActionBar?.title = title
+                }
+            }
+        })
+    }
+
+    private fun setupBottomNavigationView() {
+        bottomNavigationView?.setOnNavigationItemSelectedListener {
+            viewPager.currentItem = when(it.itemId) {
+                R.id.navigate_home -> 0
+                R.id.navigate_log -> 1
+                R.id.navigate_setting -> 2
+                else -> 0
+            }
+            supportActionBar?.title = it.title
+
+            true
+        }
     }
 }
