9245d7373d265b38cb6d58597d9bbebb70ce907d
Raye <okps123@naver.com>
2018-11-13 17:29:19 +0900

    Implement layout for MainActivity

--- app/src/main/java/com/cheongmin/voicereader/MainActivity.kt
+++ app/src/main/java/com/cheongmin/voicereader/MainActivity.kt
@@ -2,9 +2,7 @@ package com.cheongmin.voicereader
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentPagerAdapter
-import android.support.v4.view.ViewPager
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,62 +12,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setupActionBar()
-        setupViewPager()
-        setupBottomNavigationView()
+
+        setupProfile()
+        setupWelcomeMessage()
+        setupInformation()
     }
 
     private fun setupActionBar() {
-        setSupportActionBar(toolBar)
+        setSupportActionBar(toolbar)
 
         supportActionBar?.run {
+            title="홈"
             setDisplayShowTitleEnabled(true)
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_menu)
+            setDisplayHomeAsUpEnabled(false)
         }
     }
 
-    private fun setupViewPager() {
-        viewPager?.adapter = object: FragmentPagerAdapter(supportFragmentManager) {
-            override fun getItem(position: Int): Fragment {
-                when(position) {
-                    0 -> return MainHomeFragment.getInstance()
-                    1 -> return MainLogFragment.getInstance()
-                    2 -> return MainSettingFragment.getInstance()
-                }
-
-                return MainHomeFragment.getInstance()
-            }
-
-            override fun getCount(): Int = 3
-        }
-
-        viewPager?.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
-            override fun onPageScrollStateChanged(state: Int) {
-            }
-
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-            }
-
-            override fun onPageSelected(position: Int) {
-                bottomNavigationView.menu.getItem(position).apply {
-                    isChecked = true
-                    supportActionBar?.title = title
-                }
-            }
-        })
+    private fun setupProfile() {
+        //TODO: 사용자 프로필 사진의 주소를 로컬DB 혹은 서버로부터 받아오도록 수정해야함
+        Picasso.get()
+                .load("https://yt3.ggpht.com/-yOlMtZbsU4g/AAAAAAAAAAI/AAAAAAAAAAA/6W9wp1WvxIg/s76-c-k-no-mo-rj-c0xffffff/photo.jpg")
+                .into(iv_user_profile)
     }
 
-    private fun setupBottomNavigationView() {
-        bottomNavigationView?.setOnNavigationItemSelectedListener {
-            viewPager.currentItem = when(it.itemId) {
-                R.id.navigate_home -> 0
-                R.id.navigate_log -> 1
-                R.id.navigate_setting -> 2
-                else -> 0
-            }
-            supportActionBar?.title = it.title
+    private fun setupWelcomeMessage() {
+        //TODO: 사용자의 이름과 알람 상태 등을 바탕으로 환영 메세지를 화면에 출력해야함
+        val userName = "개발자"
+        var message = userName + "님 안녕하세요\n"
+        message += "1개의 답변이 달렸습니다"
 
-            true
-        }
+        tv_welcome_message.text = message
+    }
+
+    private fun setupInformation() {
+        //TODO: 알림 개수와 활동 점수를 로컬 DB와 서버로부터 받아오도록 수정해야함
+        val notification_count = 1
+        val action_point = 130
+
+        tv_notification_count.text = notification_count.toString()
+        tv_action_point.text = action_point.toString()
     }
 }
