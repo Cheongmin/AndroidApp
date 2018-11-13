88a2a5848696e9e7b3eecce57d5fbc1f0adb5721
Raye <okps123@naver.com>
2018-11-14 01:07:26 +0900

    Implement record layout in PostQuestionActivity Remove unused icon

--- app/src/main/java/com/cheongmin/voicereader/PostQuestionActivity.kt
+++ app/src/main/java/com/cheongmin/voicereader/PostQuestionActivity.kt
@@ -2,6 +2,11 @@ package com.cheongmin.voicereader
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_post_question.*
 
 class PostQuestionActivity : AppCompatActivity() {
@@ -11,6 +16,22 @@ class PostQuestionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_post_question)
 
         setupActionBar()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_post_question, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.menu_item_post_question_post -> {
+                Toast.makeText(applicationContext, "Call Upload", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setupActionBar() {
