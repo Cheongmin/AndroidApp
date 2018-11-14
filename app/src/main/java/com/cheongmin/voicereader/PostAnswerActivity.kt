de5ebf48ec556153fa833d31a5ca74cc52fc05be
Raye <okps123@naver.com>
2018-11-14 20:46:21 +0900

    Impelment record player layout in PostQuestionActivity Add PostAnswerActivity

---
+++ app/src/main/java/com/cheongmin/voicereader/PostAnswerActivity.kt
@@ -0,0 +1,12 @@
+package com.cheongmin.voicereader
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PostAnswerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_answer)
+    }
+}
