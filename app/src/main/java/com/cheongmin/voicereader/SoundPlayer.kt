8fb54f43acb63fa752fadeca7afbf12bd178905d
Raye <okps123@naver.com>
2018-11-22 14:40:08 +0900

    Implement zoom in/out and expand subtitle in soundplayer

--- app/src/main/java/com/cheongmin/voicereader/SoundPlayer.kt
+++ app/src/main/java/com/cheongmin/voicereader/SoundPlayer.kt
@@ -3,12 +3,19 @@ package com.cheongmin.voicereader
 import android.animation.ValueAnimator
 import android.content.Context
 import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.layout_sound_player.view.*
 import android.support.v4.view.animation.FastOutSlowInInterpolator
+import android.transition.ChangeBounds
+import android.transition.TransitionManager
+import android.util.Log
 import android.util.TypedValue
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.BounceInterpolator
+import android.widget.Toast
 
 
 class SoundPlayer
@@ -21,7 +28,7 @@ class SoundPlayer
         }
 
     private val expandedSubtitleLayoutHeight : Int =
-            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 80.0f, resources.displayMetrics).toInt()
+            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100.0f, resources.displayMetrics).toInt()
 
     private var isExpanded = false
 
@@ -54,24 +61,26 @@ class SoundPlayer
         btn_toggle_subtitle.setOnClickListener {
             toggleSubtitle()
         }
+
+        btn_zoom_in_subtitle.setOnClickListener {
+            tv_subtitles.setTextSize(TypedValue.COMPLEX_UNIT_PX, tv_subtitles.textSize+8)
+        }
+
+        btn_zoom_out_subtitle.setOnClickListener {
+            tv_subtitles.setTextSize(TypedValue.COMPLEX_UNIT_PX, tv_subtitles.textSize-8)
+        }
     }
 
     private fun toggleSubtitle() {
         isExpanded = !isExpanded
 
-        val startValue = sv_subtitles.measuredHeight
-        val endValue = if (isExpanded) expandedSubtitleLayoutHeight else 0
-
-        var animator = ValueAnimator.ofInt(startValue, endValue)
-        animator.interpolator = FastOutSlowInInterpolator()
-
-        animator.addUpdateListener {
-            val params = sv_subtitles.layoutParams
-            params.height = it.animatedValue as Int
-
-            sv_subtitles.layoutParams = params
+        val constraint = ConstraintSet()
+        if (isExpanded) {
+            constraint.clone(context, R.layout.layout_sound_player_expanded)
+        } else {
+            constraint.clone(context, R.layout.layout_sound_player)
         }
 
-        animator.setDuration(500).start()
+        constraint.applyTo(root)
     }
 }
