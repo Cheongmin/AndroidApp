4f59c5c6302393e7321199dec9ab4782499c565e
pjh <pjh9996@kookmin.ac.kr>
2018-11-24 21:36:32 +0900

    [Retrofit] Authorization Header 추가요청 구현
    테스트는 안해봄

--- app/src/main/java/com/cheongmin/voicereader/network/RetrofitTestActivity.kt
+++ app/src/main/java/com/cheongmin/voicereader/network/RetrofitTestActivity.kt
@@ -5,11 +5,18 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.cheongmin.voicereader.model.User
 import com.cheongmin.voicereader.R
+import com.cheongmin.voicereader.model.Question
+import com.cheongmin.voicereader.model.QuestionRequest
+import com.cheongmin.voicereader.service.QuestionService
 import com.cheongmin.voicereader.service.UserService
 import kotlinx.android.synthetic.main.activity_retrofit.*
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.File
 
 
 class RetrofitTestActivity : AppCompatActivity() {
@@ -17,7 +24,7 @@ class RetrofitTestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_retrofit)
 
-        btn_request.setOnClickListener {
+        /*btn_request.setOnClickListener {
             val apiClient = RetrofitManager.create(UserService::class.java)
             apiClient.fetchUser("5bf7647613a29200014e9a10")
                     .enqueue(object : Callback<User> {
@@ -34,26 +41,27 @@ class RetrofitTestActivity : AppCompatActivity() {
                             t.printStackTrace()
                         }
                     })
-        }
-    }
-}
+        }*/
 
-        /*val file = File("/storage/emulated/0/Download/1.wav")
-        val sound = MultipartBody.Part.createFormData("sound",file.getName(), RequestBody.create(MediaType.parse("audio/*"), file))
+        val file = File("/storage/emulated/0/Download/1.wav")
+        val sound = MultipartBody.Part.createFormData("sound", file.getName(), RequestBody.create(MediaType.parse("audio/*"), file))
         val json = MultipartBody.Part.createFormData("json", QuestionRequest("testsubtitle", "testcontents").toString())
 
         btn_request.setOnClickListener {
-            val apiClient = RetrofitManager.create(QuestionService::class.java)
+            val apiClient = RetrofitManager.createWithAccessToken(QuestionService::class.java)
             apiClient.newQuestion(sound, json)
-                    .enqueue(object: Callback<Question> {
+                    .enqueue(object : Callback<Question> {
                         override fun onResponse(call: Call<Question>, response: Response<Question>) {
                             Log.d("Retrofit", "call onResponse")
                             Log.d("Retrofit", response.message())
                             Log.d("Retrofit", response.body().toString())
                         }
+
                         override fun onFailure(call: Call<Question>, t: Throwable) {
                             Log.d("Retrofit", "call onFailure")
                             t.printStackTrace()
                         }
                     })
-        }*/
+        }
+    }
+}
