578c6c28725b6005d8ee23664f1278fa0fe1a250
Raye <okps123@naver.com>
2019-01-31 15:56:40 +0900

    Implement login failure alert message

--- app/src/main/java/com/cheongmin/voicereader/view/LoginActivity.kt
+++ app/src/main/java/com/cheongmin/voicereader/view/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.cheongmin.voicereader.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.androidhuman.rxfirebase2.auth.*
@@ -10,10 +11,12 @@ import com.cheongmin.voicereader.R
 import com.cheongmin.voicereader.api.AuthorizationAPI
 import com.cheongmin.voicereader.api.UserAPI
 import com.cheongmin.voicereader.model.response.AccessToken
+import com.cheongmin.voicereader.model.response.User
 import com.cheongmin.voicereader.network.TokenManager
 import com.cheongmin.voicereader.network.UserManager
 import com.cheongmin.voicereader.network.client.ApiClient
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -30,14 +33,20 @@ class LoginActivity : AppCompatActivity() {
       var password = edit_password.text.toString()
 
       //TODO: Implement Validate Email and Password
+      edit_email.isEnabled = false
+      edit_password.isEnabled = false
+      btn_login.isEnabled = false
 
       handleLogin(email, password)
     }
   }
 
   private fun handleLogin(email: String, password: String) {
-    val tokenManager = TokenManager.getInstance(applicationContext)
+    edit_email.isEnabled = false
+    edit_password.isEnabled = false
+    btn_login.isEnabled = false
 
+    val tokenManager = TokenManager.getInstance(applicationContext)
     FirebaseAuth.getInstance().rxSignInWithEmailAndPassword(email, password)
       .subscribeOn(Schedulers.io())
       .observeOn(AndroidSchedulers.mainThread())
@@ -53,12 +62,28 @@ class LoginActivity : AppCompatActivity() {
 
         return@flatMap UserAPI.fetchUser(userId!!)
       }
-      .subscribe({
-        UserManager.user = it
-        navigateToMainActivity()
-      }, {
-        throw it
-      })
+      .subscribe(this::handleLoginSuccess, this::handleLoginFailure)
+  }
+
+  private fun handleLoginSuccess(user: User) {
+    UserManager.user = user
+    navigateToMainActivity()
+  }
+
+  private fun handleLoginFailure(throwable: Throwable) {
+    AlertDialog.Builder(this, R.style.VoiceReader_AlertDialog).apply {
+      setTitle("로그인 실패")
+      setMessage("이메일 또는 비밀번호를 다시 확인해주세요!")
+      setCancelable(false)
+      setPositiveButton("확인") { dialog, _ ->
+        dialog.dismiss()
+      }
+      show()
+    }
+
+    edit_email.isEnabled = true
+    edit_password.isEnabled = true
+    btn_login.isEnabled = true
   }
 
   private fun navigateToMainActivity() {
